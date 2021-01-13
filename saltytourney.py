8c857931484f5f10ac6340fa622619fc1b828169
granite.crow <granite.crow@gmail.com>
2021-01-12 21:56:48 -0500

    added match simulation for each round based on fighter probability

--- saltytourney.py
+++ saltytourney.py
@@ -2,6 +2,7 @@ import requests
 import pandas as pd
 import numpy as np
 from bs4 import BeautifulSoup
+import random
 
 #local imports
 import authenticate
@@ -11,43 +12,6 @@ import saltysite
 import config
 import saltydb
 from elo import *
-
-html_string = """
-    <table id="bracket" style="margin: 0 auto;border-collapse:initial;">
-            <tr><th>Round 1</th><th>Round 2</th><th>Round 3</th><th>Final</th><th>Winner</th></tr>
-            <tr><td>Evil ryu sf3</td><td></td><td></td><td></td><td></td></tr>
-            <tr><td></td><td>&nbsp;Evil ryu sf3</td><td></td><td></td><td></td></tr>
-            <tr><td>Turbo ken</td><td></td><td></td><td></td><td></td></tr>
-            <tr><td></td><td></td><td>&nbsp;</td><td></td><td></td></tr>
-            <tr><td>Evil ryu svc chaos</td><td></td><td></td><td></td><td></td></tr>
-            <tr><td></td><td>&nbsp;Evil ryu svc chaos</td><td></td><td></td><td></td></tr>
-            <tr><td>E Ken</td><td></td><td></td><td></td><td></td></tr>
-            <tr><td></td><td></td><td></td><td>&nbsp;</td><td></td></tr>
-            <tr><td>Mahvel ryu</td><td></td><td></td><td></td><td></td></tr>
-            <tr><td></td><td>&nbsp;Mahvel ryu</td><td></td><td></td><td></td></tr>
-            <tr><td>Evil ken</td><td></td><td></td><td></td><td></td></tr>
-            <tr><td></td><td></td><td>&nbsp;</td><td></td><td></td></tr>
-            <tr><td>Most evil ryu</td><td></td><td></td><td></td><td></td></tr>
-            <tr><td></td><td>&nbsp;Ken svc chaos</td><td></td><td></td><td></td></tr>
-            <tr><td>Ken svc chaos</td><td></td><td></td><td></td><td></td></tr>
-            <tr><td></td><td></td><td></td><td></td><td>&nbsp;</td></tr>
-            <tr><td>Omega god ryu</td><td></td><td></td><td></td><td></td></tr>
-            <tr><td></td><td>&nbsp;Omega god ryu</td><td></td><td></td><td></td></tr>
-            <tr><td>Mahvel ken</td><td></td><td></td><td></td><td></td></tr>
-            <tr><td></td><td></td><td>&nbsp;</td><td></td><td></td></tr>
-            <tr><td>Pocket evil ryu</td><td></td><td></td><td></td><td></td></tr>
-            <tr><td></td><td>&nbsp;Broken</td><td></td><td></td><td></td></tr>
-            <tr><td>Broken</td><td></td><td></td><td></td><td></td></tr>
-            <tr><td></td><td></td><td></td><td>&nbsp;</td><td></td></tr>
-            <tr><td>Rainbow edition ryu</td><td></td><td></td><td></td><td></td></tr>
-            <tr><td></td><td>&nbsp;<span class="greentext">Pending</span></td><td></td><td></td><td></td></tr>
-            <tr><td>Ssf2x ken</td><td></td><td></td><td></td><td></td></tr>
-            <tr><td></td><td></td><td>&nbsp;</td><td></td><td></td></tr>
-            <tr><td>Ryu EX3</td><td></td><td></td><td></td><td></td></tr>
-            <tr><td></td><td>&nbsp;</td><td></td><td></td><td></td></tr>
-            <tr><td>Ryu mvc1</td><td></td><td></td><td></td><td></td></tr>
-        </table>
-"""
 
 def parse_url(url):
     response = requests.get(url)
@@ -95,9 +59,11 @@ def lookup_ELO(db, fighter):
 
 def determine_match_winners(fighters):
     winners = []
+    sim_games = 9999
     for i in zip(fighters[0::2], fighters[1::2]):
         (Pa, Pb) = ExpectedProbabilities(i[0][1], i[1][1])
-        if (Pa > Pb):
+        play_game = random.randrange(0, sim_games, 1)
+        if(play_game < (Pa * sim_games)):
             Pa = Pa * 100
             print(f'Fighter: {i[0][0]} | {Pa:.0f}%')
             winners.append(i[0])
@@ -114,9 +80,6 @@ def main():
     table = parse_url(config.TOURNEY_URL)
     df = parse_table(table)
 
-    fighter_list = df['Round 1'].tolist()
-    fighter_list = list(filter(lambda x: x != '', fighter_list))
-
     fighters = df[df['Round 1'] != ""].loc[:,'Round 1'].values
     fighters = [(fighter,lookup_ELO(db, fighter)) for fighter in fighters]
 
@@ -129,27 +92,28 @@ def main():
     print('\nChampion')
     champion = determine_match_winners(third_rnd_winners)
 
+    df_projected = df.copy()
 
-    df['Round 2'][1] = first_rnd_winners[0][0]
-    df['Round 2'][5] = first_rnd_winners[1][0]
-    df['Round 2'][9] = first_rnd_winners[2][0]
-    df['Round 2'][13] = first_rnd_winners[3][0]
-    df['Round 2'][17] = first_rnd_winners[4][0]
-    df['Round 2'][21] = first_rnd_winners[5][0]
-    df['Round 2'][25] = first_rnd_winners[6][0]
-    df['Round 2'][29] = first_rnd_winners[7][0]
+    df_projected['Round 2'][1] = first_rnd_winners[0][0]
+    df_projected['Round 2'][5] = first_rnd_winners[1][0]
+    df_projected['Round 2'][9] = first_rnd_winners[2][0]
+    df_projected['Round 2'][13] = first_rnd_winners[3][0]
+    df_projected['Round 2'][17] = first_rnd_winners[4][0]
+    df_projected['Round 2'][21] = first_rnd_winners[5][0]
+    df_projected['Round 2'][25] = first_rnd_winners[6][0]
+    df_projected['Round 2'][29] = first_rnd_winners[7][0]
 
-    df['Round 3'][3] = second_rnd_winners[0][0]
-    df['Round 3'][11] = second_rnd_winners[1][0]
-    df['Round 3'][19] = second_rnd_winners[2][0]
-    df['Round 3'][27] = second_rnd_winners[3][0]
+    df_projected['Round 3'][3] = second_rnd_winners[0][0]
+    df_projected['Round 3'][11] = second_rnd_winners[1][0]
+    df_projected['Round 3'][19] = second_rnd_winners[2][0]
+    df_projected['Round 3'][27] = second_rnd_winners[3][0]
 
-    df['Final'][7] = third_rnd_winners[0][0]
-    df['Final'][23] = third_rnd_winners[1][0]
+    df_projected['Final'][7] = third_rnd_winners[0][0]
+    df_projected['Final'][23] = third_rnd_winners[1][0]
 
-    df['Winner'][15] = champion[0][0]
-    print('\nBracket:')
-    print(df)
+    df_projected['Winner'][15] = champion[0][0]
+    print('\nProjected Bracket:')
+    print(df_projected)
 
     # logic for the table:
     # only one fighter per row
